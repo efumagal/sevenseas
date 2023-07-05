1ee699cf53e89197c14cca5f8562acd46e0221ca
efumagal <77152760+efumagal@users.noreply.github.com>
2023-07-05 08:57:35 +0100

    Added checks to port creation test (#5)

--- internal/core/domain/model_test.go
+++ internal/core/domain/model_test.go
@@ -7,12 +7,14 @@ import (
 )
 
 func TestNewPort(t *testing.T) {
+	type checks func(t *testing.T, port Port)
 	// Build our needed testcase data struct
 	type testCase struct {
-		test          string
-		id            string
-		portData      PortData
-		expectedError error
+		test             string
+		id               string
+		portData         PortData
+		expectedError    error
+		additionalChecks checks
 	}
 	// Create new test cases
 	testCases := []testCase{
@@ -46,6 +48,12 @@ func TestNewPort(t *testing.T) {
 			portData: PortData{City: "Ajman", Coordinates: []float64{55.5136433,
 				25.4052165}},
 			expectedError: nil,
+			additionalChecks: func(t *testing.T, port Port) {
+				assert.Equal(t, "AEAJM", port.ID)
+				assert.Equal(t, "Ajman", port.City)
+				assert.Len(t, port.Coordinates, 2)
+				assert.InDelta(t, 55.513643, port.Coordinates[0], 0.00001)
+			},
 		},
 	}
 
@@ -53,9 +61,13 @@ func TestNewPort(t *testing.T) {
 		// Run Tests
 		t.Run(tc.test, func(t *testing.T) {
 			// Create a new port
-			_, err := NewPort(tc.id, tc.portData)
+			port, err := NewPort(tc.id, tc.portData)
 			// Check if the error matches the expected error
 			assert.Equal(t, tc.expectedError, err)
+			// Run additional checks on created port
+			if tc.additionalChecks != nil {
+				tc.additionalChecks(t, port)
+			}
 		})
 	}
 }
